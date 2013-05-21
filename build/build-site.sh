394203bb7a23aa4f223c55670ec550a47fef5147
Jonas Pommerening <jonas.pommerening@gmail.com>
2013-05-21 12:31:56 +0200

    Nicer cursor pulse.

--- build/build-site.sh
+++ build/build-site.sh
@@ -5,7 +5,9 @@ cd `dirname "$0"`
 git clone --branch=master .. choice
 git clone git://github.com/kripken/emscripten.git emscripten
 
-./emscripten/emcc choice/choice.c choice/example.c \
-  emscripten/system/lib/dlmalloc.c \
+./emscripten/emcc \
   --pre-js em_pre.js --post-js em_post.js \
+  emscripten/system/lib/dlmalloc.c \
+  choice/choice.c \
+  choice/example.c \
   -o example.js
